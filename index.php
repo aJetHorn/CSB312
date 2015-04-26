2eb3029ec2727a991e6811c9451f19a5d4d71baf
wra216 <wellesleyarreza@ymail.com>
2015-04-26 17:26:13 -0400

    fixed delete nodes issue. modified styling for header.

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
+
+
         <?php
-        // put your code here
+        header( 'Location: ./Main/login.html' ) ;
+        echo "test";
         ?>
-    </body>
-</html>
+
